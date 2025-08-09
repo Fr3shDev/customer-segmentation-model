1e5f82b7bc5641ffaffbcae218ecce1878c07f9c
Hans Weno <billyhans90@gmail.com>
2025-08-09 14:08:31 +0100

    Visualize the data before clustering

--- src/model.py
+++ src/model.py
@@ -1,5 +1,8 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
+# Load dataset and check first 5 rows
 mall_data = pd.read_csv('data/Mall_Customers.csv')
 print(mall_data.head())
 
@@ -12,7 +15,16 @@ print('\nColumn types:\n', mall_data.dtypes)
 print('\nMissing values per column')
 print(mall_data.isna().sum())
 
-print('\nNumeric summary statistics:')
-print(mall_data.describe())
+X = mall_data[['Annual Income (k$)', 'Spending Score (1-100)']]
 
-cleaned = mall_data.dropna(subset=['Annual Income', 'Spending Score'])
+# Set Income and Score on the same scale to avoid bias
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+plt.scatter(X['Annual Income (k$)'], X['Spending Score (1-100)'], alpha=0.6)
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.title('Customers Income vs Spending Score')
+plt.show()
+
+
